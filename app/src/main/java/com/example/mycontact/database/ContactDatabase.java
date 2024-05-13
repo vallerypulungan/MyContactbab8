392696eb5f74d57d2f5870501100e76083acfb6f
vallerypulungan <vallerypulungan@student.ub.ac.id>
2024-05-13 20:30:58 +0700

    bab8 added

---
+++ app/src/main/java/com/example/mycontact/database/ContactDatabase.java
@@ -0,0 +1,12 @@
+package com.example.mycontact.database;
+
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+import com.example.mycontact.model.Contact;
+
+@Database(entities = {Contact.class}, version = 1)
+public abstract class ContactDatabase extends RoomDatabase {
+
+    public abstract ContactDao contactDao();
+}
